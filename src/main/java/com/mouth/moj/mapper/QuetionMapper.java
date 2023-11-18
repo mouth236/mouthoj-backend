8255d3189465a8af9529e1cf1fb8ae78d1538592
waima <2283755639@qq.com>
2023-11-18 21:58:00 +0800

    完善了题目表和题目提交表sql结构，用mybatis-plus生成基本的CRUD,套用模板更改为业务所需内容，编写了需要的枚举类，

---
+++ src/main/java/com/mouth/moj/mapper/QuetionMapper.java
@@ -0,0 +1,18 @@
+package com.mouth.moj.mapper;
+
+import com.mouth.moj.model.entity.Question;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Lenovo
+* @description 针对表【quetion(题目表)】的数据库操作Mapper
+* @createDate 2023-11-17 22:03:55
+* @Entity com.mouth.moj.model.entity.Quetion
+*/
+public interface QuetionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
